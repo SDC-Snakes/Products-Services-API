7629a35b2152b4eab9f47b2127d3aa2f1a4c56f9
Kyle Stevens <kyleastevens3@gmail.com>
2023-04-06 19:03:06 -0700

    Got first route working

--- server/db.js
+++ server/db.js
@@ -1,18 +1,17 @@
 // const { Pool } = require('pg');
-const pgp = require('pg-promise');
+const pgp = require('pg-promise')();
 // const path = require('path');
 // const fs = require('fs');
 
 const cn = {
-  host: 'localhost',
-  port: 3000,
+  // host: 'localhost',
+  port: 5432,
   database: 'products',
-  user: 'root',
-  password: '',
+  // user: 'kylestevens',
   max: 30,
 };
 
-const db = pgp('postgres://root:@localhost:3000/products');
+const db = pgp(cn);
 
 // const schemaPath = path.join(__dirname, 'schema.sql');
 // const schemaQuery = fs.readFileSync(schemaPath).toString();
