e7c8463170421ccf92949a78776b06bfa4c26c35
Kyle Stevens <kyleastevens3@gmail.com>
2023-04-13 14:45:34 -0700

    added loader io verification

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const routes = require('./routes');
 // require('dotenv').config();
 
@@ -6,6 +7,10 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.get('/loaderio-989755a2fdb5f1783c3abc2a85710ccd', (req, res) => {
+  res.send('loaderio-989755a2fdb5f1783c3abc2a85710ccd');
+});
+app.use(morgan('dev'));
 app.use('/products', routes);
 
 app.listen(port, () => {
